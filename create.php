ebb243e11d52333763d83a951e278a1e629f83b6
tfolly <tfolly@student.42.fr>
2016-06-18 13:53:30 +0200

    refaire les bd

--- create.php
+++ create.php
@@ -3,14 +3,8 @@
 if ($_POST['login'] && $_POST['passwd'])
 {
 
-	if (!file_exists("../private"))
-		mkdir("../private", 0777, true);
+	$finename = "../private/userbd";
 
-	$finename = "../private/passwd";
-	if (file_exists($finename))
-		$data_file = file_get_contents($finename, true);
-	else
-		$data_file = "";
 	$data_file = unserialize($data_file);
 
 	if ($data_file[$_POST['login']])
