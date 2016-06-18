fa85a3df4f0db4629dcc47cc3c3f104c3979d289
tfolly <tfolly@student.42.fr>
2016-06-18 19:04:41 +0200

    add user bugged admin panel

--- admin_panel.php
+++ admin_panel.php
@@ -1,11 +1,17 @@
 <?php
 
+include 'admin_adduser.php';
+include 'admin_deluser.php';
+
 $filename = "./private/userdb";
 $data = file_get_contents($filename);
 $data =unserialize($data);
 
 if ($_POST[login])
-	echo "efface $_POST[login]";
+	echo "efface $_POST[submit]<br />";
+
+if ($_POST[newlogin] && $_POST[newpasswd])
+	admin_adduser($_POST[newlogin], $_POST[newpasswd], 0);
 
 ?>
 
@@ -17,15 +23,26 @@ if ($_POST[login])
 </header>
 
 <h2>Panneau d'Administration</h2>
-<h3> Utilisateurs </h3>
+<h3> Effacer utilisateurs </h3>
 <form action=admin_panel.php method=post>
 	<ul>
 
 <?php foreach ($data as $login)
 {
-	echo "<li class=usr><input type=checkbox>$login[login]</li>";
+	echo "<li class=usr><input type=checkbox >$login[login]</li>";
 } ?>
 	</ul>
-<input type=submit name=login value=effacer />
+<input type=submit name=del value=effacer />
 </form>
+
+
+<h3> Ajout d'utilisateur </h3>
+<form action=admin_panel.php method=post>
+	<p>Login: <input type="text" name="newlogin"><br /></p>
+	<p>Mot de passe:<input type="text" name="newpasswd"><br /></p>
+<input type=submit name=submit value=ajouter />
+</form>
+
+
+
 </html>
