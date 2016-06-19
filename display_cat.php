ad2eb7a61deff3da1f69e0e0ef81ebaf1e64fcb1
Your Name <you@example.com>
2016-06-19 12:54:35 +0200

    add ok

--- display_cat.php
+++ display_cat.php
@@ -7,17 +7,17 @@ function display_cat()
 	$data = unserialize($data);
 	$category = array("vetements", "accessoires", "bijoux");
 	// print_r($data);
-	echo "<h3>Boutique</h3>
-	<p>vetements<p>";
+	echo "<h3>Boutique</h3>";
 	//
 	// <form action='login.php' method='post'>
-	// 	<input type='submit' name='submit' value='OK' /></p>
+	// 	<input type='submit' name='name' value='OK' /></p>
 	// </form>
-
+foreach ($category as $type)
+{
+	echo "<p>$type<p>";
 			foreach ($data as $elem)
 			{
-				print_r($elem[tag]);
-				if (array_search("vetements", $elem[tag]) !== FALSE)
+				if (array_search($type, $elem[tag]) !== FALSE)
 						echo "
 						<form action=add_article.php method=POST >
 						<p>$elem[name] : $elem[price]$</p>
@@ -25,25 +25,36 @@ function display_cat()
 						<input type=submit name=article value=$elem[name]>
 						</form>
 						";
-
 			}
-
-	echo"<p>accessoires<p>";
-	foreach ($data as $elem)
-	{
-		print_r($elem[tag]);
-		if (array_search("bijoux", $elem[tag]) !== FALSE)
-				echo "
-				<form action=add_article.php method=POST >
-				<p>$elem[name] : $elem[price]$</p>
-				<img class=galery_img src=$elem[img] />
-				<input type=submit name=article value=$elem[name]>
-				</form>
-				";
-
-
-	}
 }
 
-
+	// echo"<p>accessoires<p>";
+	// foreach ($data as $elem)
+	// {
+	// 	print_r($elem[tag]);
+	// 	if (array_search("bijoux", $elem[tag]) !== FALSE)
+	// 			echo "
+	// 			<form action=add_article.php method=POST >
+	// 			<p>$elem[name] : $elem[price]$</p>
+	// 			<img class=galery_img src=$elem[img] />
+	// 			<input type=submit name=article value=$elem[name]>
+	// 			</form>
+	// 			";
+	//
+	//
+	// }
+}
+// foreach ($data as $elem)
+// {
+// 	print_r($elem[tag]);
+// 	if (array_search("vetements", $elem[tag]) !== FALSE)
+// 			echo "
+// 			<form action=add_article.php method=POST >
+// 			<p>$elem[name] : $elem[price]$</p>
+// 			<img class=galery_img src=$elem[img] />
+// 			<input type=submit name=article value=$elem[name]>
+// 			</form>
+// 			";
+//
+// }
 ?>
