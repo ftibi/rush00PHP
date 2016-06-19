d2e44779b5d1e04b4952a413169ba24231c22138
tfolly <tfolly@student.42.fr>
2016-06-19 13:49:40 +0200

    petit fix merge

--- modif.php
+++ modif.php
@@ -1,19 +1,4 @@
 <?php
-<<<<<<< HEAD
-if ($_POST['login'] && $_POST['oldpw'] && $_POST['newpw'])
-{
-	if (!file_exists("./private"))
-		mkdir("./private", 0777, true);
-	$data_file = "";
-	$finename = "./private/passwd";
-	if (file_exists($finename))
-		$data_file = file_get_contents($finename, true);
-	else
-		$data_file = "";
-	$data_file = unserialize($data_file);
-	if (!$data_file[$_POST['login']] || $data_file[$_POST['login']]['passwd'] !== hash("whirlpool",$_POST['oldpw']))
-=======
-
 include 'get_usr_data.php';
 
 if ($_POST['login'] && $_POST['oldpw'] && $_POST['newpw'])
@@ -22,17 +7,13 @@ if ($_POST['login'] && $_POST['oldpw'] && $_POST['newpw'])
 
 
 	if (!$data[$_POST['login']] || $data[$_POST['login']]['passwd'] !== hash("whirlpool",$_POST['oldpw']))
->>>>>>> tibo
 	{
 			echo ("ERROR\n");
 			exit();
 	}
 	else
 	{
-<<<<<<< HEAD
-=======
 		header('Location:./index.php');
->>>>>>> tibo
 		echo ("OK\n");
 		$data[$_POST['login']] = array("login" => $_POST['login'], "passwd" => hash("whirlpool", $_POST['newpw']));
 		$data = serialize($data);
