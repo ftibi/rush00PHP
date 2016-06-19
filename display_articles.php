d871a471afa18b26df664e9c897855e328d3acc4
Your Name <you@example.com>
2016-06-19 15:01:51 +0200

    ok

--- display_articles.php
+++ display_articles.php
@@ -7,13 +7,13 @@ function display_articles()
 	// print_r($data);
 	foreach ($data as $elem)
 	{
-		echo "
+		echo "<div class=onearticle>
+<img class=galery_img src=$elem[img] />
 <form action=add_article.php method=POST >
 <p>$elem[name] : $elem[price]$</p>
-<img class=galery_img src=$elem[img] />
-<input type=submit name=article value=$elem[name]>
+ajouter<br/><input type=submit name=article value=$elem[name]><br/>au panier
 </form>
-
+</div>
 ";
 	}
 }
---
+++ return_array_cat.php
@@ -0,0 +1,12 @@
+<?php
+function return_array_cat()
+{
+	$i = 0;
+$data = unserializedb("./private/shopdb");
+
+
+
+}
+
+
+?>
