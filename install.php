7c23e78498a59c5ccc8ebf888d3b65e28292c3c5
tfolly <tfolly@student.42.fr>
2016-06-19 11:41:49 +0200

    cat -> array

--- install.php
+++ install.php
@@ -7,14 +7,14 @@ function install()
 	$shopdb = "./private/shopdb";
 
 	// echo"<br />user init<br />";
-	$userinit = array("tibo" => array("login" => "tibo", "passwd" => hash("whirlpool", "moi"), "admin" => 1, "panier" => array("montre" => array("name" => "montre", "price" => 50, "quantity" => 1,"img" => "./img/montre.jpg", "tag" => "accessoires"))),
+	$userinit = array("tibo" => array("login" => "tibo", "passwd" => hash("whirlpool", "moi"), "admin" => 1, "panier" => array("montre" => array("name" => "montre", "price" => 50, "quantity" => 1,"img" => "./img/montre.jpg", "tag" => array("accessoires", "bijoux")))),
 					"david" => array("login" => "david", "passwd" => hash("whirlpool", "moi"), "admin" => 1, "panier" => array()),
 					"toto" => array("login" => "toto", "passwd" => hash("whirlpool", "moi"), "admin" => 0, "panier" => array()));
 	// var_dump($userinit);
 
 	// echo"<br />shop init<br />";
-	$shopinit = array("tshirt" => array("name" => "tshirt", "price" => 20, "quantity" => 10,"img" => "./img/tshirt.jpg", "tag" => "vetements"),
-						"montre" => array("name" => "montre", "price" => 50, "quantity" => 10,"img" => "./img/montre.jpg", "tag" => "accessoires"));
+	$shopinit = array("tshirt" => array("name" => "tshirt", "price" => 20, "quantity" => 10,"img" => "./img/tshirt.jpg", "tag" => array("vetements")),
+						"montre" => array("name" => "montre", "price" => 50, "quantity" => 10,"img" => "./img/montre.jpg", "tag" => array("accessoires", "bijoux")));
 	// var_dump($shopinit);
 	if (!file_exists("./private"))
 		mkdir("./private", 0777, true);
