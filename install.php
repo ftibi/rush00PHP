db76f4c010f6fcf07613ceb8c49063718e2c1a1a
tfolly <tfolly@student.42.fr>
2016-06-19 17:31:40 +0200

    new prices yay

--- install.php
+++ install.php
@@ -14,22 +14,22 @@ function install()
 	// var_dump($userinit);
 
 	// echo"<br />shop init<br />";
-	$shopinit = array("tshirt" => array("name" => "tshirt", "price" => 20, "quantity" => 10,"img" => "./img/tshirt.jpg", "tag" => array("vetements")),
-					"pull" => array("name" => "pull", "price" => 10, "quantity" => 10,"img" => "./img/pull.jpg", "tag" => array("vetements")),
+	$shopinit = array("tshirt" => array("name" => "tshirt", "price" => 10, "quantity" => 10,"img" => "./img/tshirt.jpg", "tag" => array("vetements")),
+					"pull" => array("name" => "pull", "price" => 20, "quantity" => 10,"img" => "./img/pull.jpg", "tag" => array("vetements")),
 					"espadrilles" => array("name" => "espadrilles", "price" => 5, "quantity" => 10,"img" => "./img/espadrilles.jpg", "tag" => array("vetements", "chaussures")),
-					"sneakers" => array("name" => "sneakers", "price" => 10, "quantity" => 10,"img" => "./img/sneakers.jpg", "tag" => array("vetements", "chaussures")),
-					"chaussures" => array("name" => "chaussures", "price" => 10, "quantity" => 10,"img" => "./img/chaussures.jpg", "tag" => array("vetements", "chaussures")),
-					"manteau" => array("name" => "manteau", "price" => 10, "quantity" => 10,"img" => "./img/manteau.jpg", "tag" => array("vetements")),
-					"parfum" => array("name" => "parfum", "price" => 10, "quantity" => 10,"img" => "./img/parfum.jpg", "tag" => array("accessoires")),
-					"collier" => array("name" => "collier", "price" => 10, "quantity" => 10,"img" => "./img/collier.jpg", "tag" => array("accessoires", "bijoux")),
+					"sneakers" => array("name" => "sneakers", "price" => 90, "quantity" => 10,"img" => "./img/sneakers.jpg", "tag" => array("vetements", "chaussures")),
+					"chaussures" => array("name" => "chaussures", "price" => 130, "quantity" => 10,"img" => "./img/chaussures.jpg", "tag" => array("vetements", "chaussures")),
+					"manteau" => array("name" => "manteau", "price" => 100, "quantity" => 10,"img" => "./img/manteau.jpg", "tag" => array("vetements")),
+					"parfum" => array("name" => "parfum", "price" => 60, "quantity" => 10,"img" => "./img/parfum.jpg", "tag" => array("accessoires")),
+					"collier" => array("name" => "collier", "price" => 5, "quantity" => 10,"img" => "./img/collier.jpg", "tag" => array("accessoires", "bijoux")),
 					"bracelet" => array("name" => "bracelet", "price" => 10, "quantity" => 10,"img" => "./img/bracelet.jpg", "tag" => array("accessoires", "bijoux")),
-					"trotinette" => array("name" => "trotinette", "price" => 10, "quantity" => 10,"img" => "./img/trotinette.jpg", "tag" => array("loisir")),
-					"skateboard" => array("name" => "skateboard", "price" => 10, "quantity" => 10,"img" => "./img/skateboard.jpg", "tag" => array("loisir")),
-					"costume" => array("name" => "costume", "price" => 10, "quantity" => 10,"img" => "./img/costume.jpg", "tag" => array("vetements")),
-					"sac à dos" => array("name" => "sac à dos", "price" => 10, "quantity" => 10,"img" => "./img/sacados.jpg", "tag" => array("bagagerie")),
-					"sac à main" => array("name" => "sac à main", "price" => 10, "quantity" => 10,"img" => "./img/sacamain.jpg", "tag" => array("accessoires", "bagagerie")),
-					"télévision" => array("name" => "télévision", "price" => 10, "quantity" => 10,"img" => "./img/television.jpg", "tag" => array("hitech")),
-					"chaine Hi-Fi" => array("name" => "chaine Hi-Fi", "price" => 10, "quantity" => 10,"img" => "./img/hifi.jpg", "tag" => array("hitech")),
+					"trotinette" => array("name" => "trotinette", "price" => 30, "quantity" => 10,"img" => "./img/trotinette.jpg", "tag" => array("loisir")),
+					"skateboard" => array("name" => "skateboard", "price" => 40, "quantity" => 10,"img" => "./img/skateboard.jpg", "tag" => array("loisir")),
+					"costume" => array("name" => "costume", "price" => 300, "quantity" => 10,"img" => "./img/costume.jpg", "tag" => array("vetements")),
+					"sac à dos" => array("name" => "sac à dos", "price" => 60, "quantity" => 10,"img" => "./img/sacados.jpg", "tag" => array("bagagerie")),
+					"sac à main" => array("name" => "sac à main", "price" => 90, "quantity" => 10,"img" => "./img/sacamain.jpg", "tag" => array("accessoires", "bagagerie")),
+					"télévision" => array("name" => "télévision", "price" => 200, "quantity" => 10,"img" => "./img/television.jpg", "tag" => array("hitech")),
+					"chaine Hi-Fi" => array("name" => "chaine Hi-Fi", "price" => 80, "quantity" => 10,"img" => "./img/hifi.jpg", "tag" => array("hitech")),
 					"montre" => array("name" => "montre", "price" => 50, "quantity" => 10,"img" => "./img/montre.jpg", "tag" => array("accessoires", "bijoux")));
 	// var_dump($shopinit);
 
